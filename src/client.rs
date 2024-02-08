9dd5d9b852011ed70547e2e5b8cd032711296d5f
PhucBB <phuc.bui@agoda.com>
2024-02-08 22:57:31 +0700

    Added client

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-    
+use myapp::{InviteReponse, InviteRequest};
+use myapp::chat_client::{ChatClient};
+pub mod myapp {
+    tonic::include_proto!("myapp");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = ChatClient::connect("http://[::1]:50051").await?;
+    let request = tonic::Request::new(InviteRequest {
+        name: "Tonic".into(),
+    });
+    let res = client.send(request).await?;
+    println!("RESPONSE={:?}", res);
+    Ok(())
 }
